9d9bcd4686cbbe711c025330cf1360e785857b4c
stevelouis4848 <33739350+stevelouis4848@users.noreply.github.com>
2018-11-20 16:51:26 -0500

    Just comments to understand assignment

---
+++ CS2EpicFight.java
@@ -0,0 +1,19 @@
+package Main_Code;
+
+public class CS2EpicFight {
+	
+	public void main(){
+		
+		
+	}
+	
+	public static int Algo(){
+		// Take the time that the fight took divided by 2 and get number of moves each
+		// You need enough time for the hits to affect so
+		// Take possible moves to choose from and build a storage for the moves that get Ruffus defeated Xavier positive
+		//  Personal stamina cost is instantaneous 
+		// opponent stamina cost takes one second
+		// opponent stamina and moves take place at the same time except for time 0.
+		return 0;
+	}
+}
